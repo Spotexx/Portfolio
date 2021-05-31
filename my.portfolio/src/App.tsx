663aeab9ca59c4206d3f209f8fa9e31080d39a7b
Spotexx <travesimo@gmail.com>
2021-05-30 20:34:58 -0400

    added template portrait and finished jss for portrait and background colors

--- my.portfolio/src/App.tsx
+++ my.portfolio/src/App.tsx
@@ -1,6 +1,7 @@
 import { Box, makeStyles } from '@material-ui/core';
 import React from 'react';
 import './App.css';
+import face from './face.png';
 
 const useStyles = makeStyles(theme => ({
   siteBackground: {
@@ -8,11 +9,30 @@ const useStyles = makeStyles(theme => ({
     flexDirection:'column',
     minHeight:'100vh',
     minWidth:'100vw',
-    background: theme.palette.primary.main
+    // why is test appearing above the portrait #line 46
    },
+  content: {
+    background: theme.palette.primary.main
+  },
   title: {
      background: theme.palette.primary.light
-   }
+  },
+  frame: {
+    display: 'inline',
+    position: 'relative'
+  },
+  portrait: {
+    maxHeight: '100%',
+    maxWidth: '100%',
+    width: '20vh',
+    height: 'auto',
+    position: 'absolute',
+    top: 0,
+    bottom: '40%',
+    left: 0,
+    right: 0,
+    margin: 'auto'
+  }
 }))
 
 export const App = () => {
@@ -20,13 +40,13 @@ export const App = () => {
   return (
     <Box className={classes.siteBackground}>
       <Box 
-        className={classes.title}
+        className={`${classes.title} ${classes.frame}`}
         flexGrow={1}
       >
-        test
+        <img src={face} className={classes.portrait} alt="face" />
       </Box>
-      <Box>
-        <p>test</p><p>test</p>
+      <Box className={classes.content}>
+        <p>test</p>
       </Box>
     </Box>
   );
