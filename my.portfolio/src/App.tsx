24216cb96bd1f9369921a010305156610c419bcc
Spotexx <travesimo@gmail.com>
2021-05-31 00:15:02 -0400

    added icons and appBar. responsive design for phone works at this point too

--- my.portfolio/src/App.tsx
+++ my.portfolio/src/App.tsx
@@ -1,7 +1,8 @@
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, makeStyles, Grid } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 import face from './face.png';
+import { AccountCircle, Folder, LinkedIn, GitHub ,Description, Brightness4, Home } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
   siteBackground: {
@@ -9,7 +10,6 @@ const useStyles = makeStyles(theme => ({
     flexDirection:'column',
     minHeight:'100vh',
     minWidth:'100vw',
-    // why is test appearing above the portrait #line 46
    },
   content: {
     background: theme.palette.primary.main
@@ -28,10 +28,19 @@ const useStyles = makeStyles(theme => ({
     height: 'auto',
     position: 'absolute',
     top: 0,
-    bottom: '40%',
+    bottom: '20%',
     left: 0,
     right: 0,
     margin: 'auto'
+  },
+  routerGrid: {
+    position: 'absolute',
+    maxWidth: '80%',
+    left: '0',
+    right: '0',
+    margin: 'auto',
+    textAlign: "center",
+    bottom: 0,//USING grid for the formatting router  ALSO look into elevating this off page
   }
 }))
 
@@ -44,9 +53,24 @@ export const App = () => {
         flexGrow={1}
       >
         <img src={face} className={classes.portrait} alt="face" />
+        <Grid 
+          className={classes.routerGrid}
+          container
+          direction="row"
+          justify="center"
+          spacing={3}
+        >
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><AccountCircle /></Grid>
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><Folder /></Grid>
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><LinkedIn /></Grid>
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><GitHub /></Grid>
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><Description /></Grid>
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><Brightness4 /></Grid>
+          <Grid item xs={4} sm={4} md={4} lg={2} xl={2} spacing ={4}><Home /></Grid>
+        </Grid>
       </Box>
       <Box className={classes.content}>
-        <p>test</p>
+        <p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p>
       </Box>
     </Box>
   );
