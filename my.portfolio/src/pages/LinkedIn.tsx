5296e7d9db6c25456b9882cfa0bc1ecab31da37d
Spotexx <tsanderson.023@gmail.com>
2022-03-07 22:01:04 -0500

    updated elevator pitch

--- my.portfolio/src/pages/LinkedIn.tsx
+++ my.portfolio/src/pages/LinkedIn.tsx
@@ -7,17 +7,16 @@ export const LinkedIn = () => {
     return (
         <Box sx={{ ...flexWrapperOuterStyle, ...backgroundStyle }}>
             <Container maxWidth='lg' sx={{ ...flexWrapperInnerStyle, alignItems: 'center', flexDirection: 'column', justifyContent: 'space-evenly' }}>
-                <Typography variant={'body1'} sx={{ padding: '30px', fontSize: '1rem' }}>
-                    They say that if you love what you do for your job then you never need to work a day in your life.
-                    When I wrote my first <span style={{ color: 'red' }}>javascript</span> function, I was ecstatic. I find manipulating <span style={{ color: 'red' }}>data</span> and growing my
-                    ability exciting. I am a <span style={{ color: 'red' }}>problem solver</span> and a <span style={{ color: 'red' }}>team player</span>. Because I have received so much
-                    assistance in my educational journey, I find myself giving back and try to help people where
-                    possible. I love <span style={{ color: 'red' }}>learning</span> new languages and seeing code in different ways. I seem to pick things up
-                    rather quickly and enjoy working on software that I can use in my life in a practical way.
+                <Typography variant={'body1'} sx={{ padding: '30px', fontSize: ['0.6rem', '1rem', '1.25rem'] }}>
+                    <p>“Find a job you enjoy doing, and you will never have to work a day in your life.” (Mark Twain)</p>
+
+                    <p>I am a <span style={{ color: 'red' }}>full-stack developer</span> and have a background in finance. I find manipulating data and growing my ability exciting. I am a <span style={{ color: 'red' }}>problem solver</span> and a team player. Because I have received so much assistance in my educational journey, I find myself giving back to people where possible. In addition to my almmost <span style={{ color: 'red' }}>two years</span> of personal experience, I have recently earned a certificate in full-stack web development from the University of Toronto. I love learning new languages and seeing code in different ways. I enjoy working on software that I can practically use in my life. I am a front-end developer. I use HTML, CSS, JS, and can build apps with Meta’s React framework.</p>
+
+                    <p>I am gravitating toward a junior front-end position with a <span style={{ color: 'red' }}>hands-on approach</span> to development. Ideally, I would like to contribute immediately while at the same time continuing to broaden my knowledge. I am looking for a company that provides a work environment that is conducive to continued <span style={{ color: 'red' }}>learning</span>, working hard, and advancing while upholding a positive work environment.</p>
                 </Typography>
                 <Divider flexItem sx={{ paddingTop: '30px' }}>Skills at a glance</Divider>
 
-                <Typography variant={'body1'} sx={{ fontSize: '2rem', textAlign: 'center', margin: '30px' }}>• HTML • CSS • Javascript • React JS • Material UI • Responsive Design • MongoDB • Express • Node •</Typography>
+                <Typography variant={'body1'} sx={{ fontSize: '2rem', textAlign: 'center', margin: '30px' }}>•HTML •CSS •Javascript •React JS •Material UI •Responsive Design •MongoDB •Express •Node</Typography>
 
             </Container>
 
