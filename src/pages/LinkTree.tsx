03ee1516b869f7bb801a6996100d1a363a83e548
Spotexx <tsanderson.023@gmail.com>
2022-04-05 20:21:06 -0400

    fixed css after scroll fix

--- src/pages/LinkTree.tsx
+++ src/pages/LinkTree.tsx
@@ -9,17 +9,17 @@ const LinkTree = () => {
 
     return (
         <Box sx={{ ...flexWrapperOuterStyle, ...backgroundStyle, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
-            <ElevatedPaper sx={{ ...flexWrapperInnerStyle, flexDirection: 'column', alignItems: 'center', width: '60%', height: '60%', padding: '20px' }}>
+            <ElevatedPaper sx={{ ...flexWrapperInnerStyle, flexDirection: 'column', alignItems: 'center', width: '60%', height: 'fit-content', padding: '20px' }}>
                 <img
                     style={{ maxHeight: '15vh', maxWidth: '15vh' }}
                     src={circleFace} alt="MedPic1"
                 />
-                <Divider variant="middle" sx={{ width: '70%', height: '30px' }} />
+                <Divider color='primary.main' variant="middle" sx={{ width: '70%', mt: '15px' }} />
                 <Box sx={{ width: '100%', height: '70%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
-                    <Button variant="contained" color="secondary" href='/resume.pdf' download style={{ width: '70%' }}>Resume</Button>
-                    <Button variant="contained" color="secondary" href="https://www.linkedin.com/in/travis-anderson023/" target='_blank' style={{ width: '70%' }}>LinkedIn</Button>
-                    <Button variant="contained" color="secondary" href="https://github.com/Travis-Anderson023" target='_blank' style={{ width: '70%' }}>Github</Button>
-                    <Button variant="contained" color="secondary" href="mailto:tsanderson.023@gmail.com" target='_blank' style={{ width: '70%' }}>Email</Button>
+                    <Button variant="contained" color="secondary" href='/resume.pdf' download style={{ width: '70%', margin: '15px' }}>Resume</Button>
+                    <Button variant="contained" color="secondary" href="https://www.linkedin.com/in/travis-anderson023/" target='_blank' style={{ width: '70%', margin: '15px' }}>LinkedIn</Button>
+                    <Button variant="contained" color="secondary" href="https://github.com/Travis-Anderson023" target='_blank' style={{ width: '70%', margin: '15px' }}>Github</Button>
+                    <Button variant="contained" color="secondary" href="mailto:tsanderson.023@gmail.com" target='_blank' style={{ width: '70%', margin: '15px' }}>Email</Button>
 
                 </Box>
             </ElevatedPaper>
