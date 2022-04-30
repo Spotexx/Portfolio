8f8529878945651f1140a08a820a879bad6a9a57
Travis-Anderson023 <tsanderson.023@gmail.com>
2022-04-30 15:22:26 -0400

    pulled all new assets into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import './App.css';
-import { Home } from './pages/Home';
-import { LinkedIn } from './pages/LinkedIn';
-import LinkTree from './pages/LinkTree';
-import { LolStats } from './pages/LolStats';
+import React from "react";
+import "./App.css";
+import { Appli } from "./pages/Appli";
+import { Home } from "./pages/Home";
+import { LinkedIn } from "./pages/LinkedIn";
+import LinkTree from "./pages/LinkTree";
+import { LolStats } from "./pages/LolStats";
 import { MedicationTracker } from "./pages/MedicationTracker";
+import { Sarabot } from "./pages/Sarabot";
 import { Technologies } from "./pages/Technologies";
-import Pagify from './react-mui-pagify/Pagify/index';
+import Pagify from "./react-mui-pagify/Pagify/index";
 
 export const App = () => {
-    return (
-        //click to auto scroll to section
-        //css star background
-        <Pagify>
-            <Home />
-            <LinkedIn />
-            <Technologies />
-            <LolStats />
-            <MedicationTracker />
-            <LinkTree />
-        </Pagify >
-    );
-}
-
+  return (
+    //click to auto scroll to section
+    //css star background
+    <Pagify>
+      <Home />
+      <LinkedIn />
+      <Technologies />
+      <Sarabot />
+      <LolStats />
+      <MedicationTracker />
+      <Appli />
+      <LinkTree />
+    </Pagify>
+  );
+};
